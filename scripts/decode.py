b3d526941c71a046bbf56811bff0ecd89417992e
Alex Birch <Birch-san@users.noreply.github.com>
2023-03-26 13:44:08 +0100

    parameterize number of hidden layers, print out various types of loss

--- scripts/decode.py
+++ scripts/decode.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 from os import listdir, path, makedirs
-from torch.nn import Linear, SiLU
-from torch import tensor, Tensor, IntTensor, FloatTensor, inference_mode
+from torch.nn import Linear, SiLU, ModuleList
+from torch import tensor, Tensor, IntTensor, FloatTensor, inference_mode, sub, zeros
 from helpers.device import get_device_type, DeviceLiteral
 import fnmatch
 import torch
 from PIL import Image
 from PIL.Image import Resampling
-from typing import List
+from typing import List, NamedTuple
 import numpy as np
 from torchvision.io import read_image, write_png
 from torchvision.transforms.functional import resize, InterpolationMode
@@ -36,18 +36,34 @@ for path_ in [weights_dir, predictions_dir, science_dir]:
 weights_path = path.join(weights_dir, "decoder3.pt")
 
 class Decoder3(Module):
-  lin: Linear
-  def __init__(self, inner_dim: int) -> None:
+  in_proj: Linear
+  hidden_layers: ModuleList
+  out_proj: Linear
+  def __init__(self, hidden_layer_count: int, inner_dim: int) -> None:
     super().__init__()
-    self.lin1 = Linear(4, inner_dim)
-    self.nonlin = SiLU()
-    self.lin2 = Linear(inner_dim, 3)
+    self.in_proj = Linear(4, inner_dim)
+    make_nonlin = SiLU
+    self.nonlin = make_nonlin()
+    self.hidden_layers = ModuleList([
+      layer for layer in (Linear(inner_dim, inner_dim), make_nonlin()) for _ in range(hidden_layer_count)
+    ])
+    self.out_proj = Linear(inner_dim, 3)
   
   def forward(self, sample: Tensor) -> Tensor:
-    sample: Tensor = self.lin1(sample)
+    sample: Tensor = self.in_proj(sample)
     sample: Tensor = self.nonlin(sample)
-    sample: Tensor = self.lin2(sample)
+    for layer in self.hidden_layers:
+      sample: Tensor = layer.forward(sample)
+    sample: Tensor = self.out_proj(sample)
     return sample
+
+  # def __init__(self, inner_dim: int) -> None:
+  #   super().__init__()
+  #   self.lin = Linear(4, 3, True)
+  
+  # def forward(self, sample: Tensor) -> Tensor:
+  #   sample: Tensor = self.lin(sample)
+  #   return sample
 
 class Mode(Enum):
   Train = auto()
@@ -57,16 +73,18 @@ mode = Mode.Train
 test_after_train=True
 resume_training=False
 
-model = Decoder3(inner_dim=12)
+model = Decoder3(hidden_layer_count=1, inner_dim=12)
 if path.exists(weights_path) and resume_training or mode is not Mode.Train:
   model.load_state_dict(torch.load(weights_path, weights_only=True))
 model = model.to(device)
 
 training_dtype = torch.float32
 
-epochs = 3000
+# epochs = 3000
+epochs = 1000
+# epochs = 400
 
-l2_loss = MSELoss(reduction='mean')
+l2_loss = MSELoss()
 l1_loss = L1Loss()
 optim = AdamW(model.parameters(), lr=5e-2)
 
@@ -93,18 +111,63 @@ def get_data() -> Dataset:
     train_outputs=train_outputs,
   )
 
-def loss_fn(input: FloatTensor, target: FloatTensor) -> FloatTensor:
-  return l2_loss(input, target) + 0.1 * (input.abs().max() - 1).clamp(min=0)**2
+@dataclass
+class LossBreakdown:
+  l2: FloatTensor
+  l1: FloatTensor
+  range: FloatTensor
+  l2_scaled: FloatTensor
+  l1_scaled: FloatTensor
+  range_scaled: FloatTensor
+
+class LossComponents(NamedTuple):
+  total_loss: FloatTensor
+  breakdown: LossBreakdown
+
+def describe_loss(loss_components: LossComponents) -> str:
+  loss, b = loss_components
+  unscaled_components = f'l2: {b.l2.abs().max().item():.2f}, l1: {b.l1.abs().max().item():.2f}, r: {b.range.abs().max().item():.2f}'
+  scaled_components = f'l2: {b.l2_scaled.abs().max().item():.2f}, l1: {b.l1_scaled.abs().max().item():.2f}, r: {b.range_scaled.abs().max().item():.2f}'
+  return f'loss: {loss.item():.2f}, [u] {unscaled_components} [s] {scaled_components}'
+
+zero = zeros(1, device=device, dtype=training_dtype)
+
+def loss_fn(input: FloatTensor, target: FloatTensor) -> LossComponents:
+  # TODO: better summing of absmax loss
+  # return l2_loss(input, target) + 0.05 * l1_loss(input, target) + 0.05 * (input.abs().max() - 1).clamp(min=0)**2
+  # return l2_loss(input, target) #+ 0.025 * (input.abs().max() - 1).clamp(min=0)**2
+  # return 0.9 * l2_loss(input, target) + 0.1 * l1_loss(input, target) #+ 0.025 * (input.abs().max() - 1).clamp(min=0)**2
+  l2 = l2_loss(input, target)
+  l2_scaled = 1. * l2
+  l1 = l1_loss(input, target)
+  l1_scaled = 0. * l1
+  range = sub(input.abs().max(dim=0).values, 1).clamp(min=0).mean()
+  range_scaled = 0. * range
+  breakdown = LossBreakdown(
+    l2=l2,
+    l1=l1,
+    range=range,
+    l2_scaled=l2_scaled,
+    l1_scaled=l1_scaled,
+    range_scaled=range_scaled,
+  )
+  total_loss: FloatTensor = l2_scaled + l1_scaled + range_scaled
+  return LossComponents(
+    total_loss=total_loss,
+    breakdown=breakdown,
+  )
 
 def train(epoch: int, dataset: Dataset):
   model.train()
   out: Tensor = model(dataset.train_inputs)
-  loss: Tensor = loss_fn(out, dataset.train_outputs)
+  loss_components = loss_fn(out, dataset.train_outputs)
+  loss, _ = loss_components
   optim.zero_grad()
   loss.backward()
   optim.step()
   if epoch % 100 == 0:
-    print(f'epoch {epoch:04d}, loss: {loss.item():.2f}, abs().max(): {out.abs().max().item():.2f}')
+    loss_desc: str = describe_loss(loss_components)
+    print(f'epoch {epoch:04d}, {loss_desc}')
 
 @inference_mode(True)
 def test():
