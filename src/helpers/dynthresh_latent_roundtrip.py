a7f65a45043a309f6e4ae3610db3dd16acbb0aa6
Alex Birch <Birch-san@users.noreply.github.com>
2023-03-27 00:52:42 +0100

    use conv-based encoder

--- src/helpers/dynthresh_latent_roundtrip.py
+++ src/helpers/dynthresh_latent_roundtrip.py
@@ -17,28 +17,32 @@ int8_half_range = int8_range / 2
 
 @no_grad()
 def approx_latents_to_rgb(decoder: Decoder, latents: FloatTensor) -> FloatTensor:
-  batch, channels, height, width = latents.shape
-  flat_channels_last: FloatTensor = latents.flatten(-2).transpose(-2,-1)
-  decoded: FloatTensor = decoder.forward(flat_channels_last)
-  unflat: FloatTensor = decoded.unflatten(-2, (height, -1))
+  # batch, channels, height, width = latents.shape
+  # flat_channels_last: FloatTensor = latents.flatten(-2).transpose(-2,-1)
+  # decoded: FloatTensor = decoder.forward(flat_channels_last)
+  # unflat: FloatTensor = decoded.unflatten(-2, (height, -1))
+  decoded: FloatTensor = decoder.forward(latents)
+  return decoded
 
 #   centered = unflat - int8_half_range
 #   normed = centered / int8_half_range
 #   images: FloatTensor = unflat.round().clamp(0, 255).to(dtype=torch.uint8).cpu().numpy()
 #   pil_images: List[Image.Image] = [Image.fromarray(image) for image in images]
-  return unflat
+  # return unflat
 
 def make_approx_latents_to_rgb(decoder: Decoder) -> LatentsToRGB:
   return partial(approx_latents_to_rgb, decoder)
 
 @no_grad()
 def approx_rgb_to_latents(encoder: Encoder, rgb: FloatTensor) -> FloatTensor:
-  batch, height, width, channels = rgb.shape
-  flat: FloatTensor = rgb.flatten(start_dim=-3, end_dim=-2)
-  decoded: FloatTensor = encoder.forward(flat)
-  channels_first: FloatTensor = decoded.transpose(-1, -2)
-  unflat: FloatTensor = channels_first.unflatten(-1, (height, -1))
-  return unflat
+  # batch, height, width, channels = rgb.shape
+  # flat: FloatTensor = rgb.flatten(start_dim=-3, end_dim=-2)
+  # encoded: FloatTensor = encoder.forward(flat)
+  # channels_first: FloatTensor = encoded.transpose(-1, -2)
+  # unflat: FloatTensor = channels_first.unflatten(-1, (height, -1))
+  # return unflat
+  encoded: FloatTensor = encoder.forward(rgb)
+  return encoded
 
 def make_approx_rgb_to_latents(encoder: Encoder) -> RGBToLatents:
   return partial(approx_rgb_to_latents, encoder)
