e6e54a11d0fd01d98072e788a2763cb022d6d3cc
Alex Birch <Birch-san@users.noreply.github.com>
2023-03-28 00:14:15 +0100

    make encoder follow same code patterns as decoder. measure validation loss.

--- scripts/encode.py
+++ scripts/encode.py
@@ -1,122 +1,236 @@
+from cv2 import Mat, cvtColor
+import cv2
 from dataclasses import dataclass
-from os import listdir, path, makedirs
-from torch.nn import Linear
-from torch import tensor, Tensor, IntTensor, inference_mode
-from helpers.device import get_device_type, DeviceLiteral
+from enum import Enum, auto
 import fnmatch
 import torch
-from PIL import Image
-from PIL.Image import Resampling
-from typing import List
-import numpy as np
-from torchvision.io import read_image, write_png
-from torchvision.transforms.functional import resize, InterpolationMode
-from torch.nn import MSELoss, Module
+from os import listdir, makedirs
+from os.path import join, exists
+from torch import Tensor, IntTensor, FloatTensor, inference_mode, sub, zeros, from_numpy, load, save, stack
+from torch.nn import Linear, SiLU, ModuleList, MSELoss, L1Loss, Module
 from torch.optim import AdamW
-from enum import Enum, auto
-from torch.nn.functional import normalize
+from torchvision.io import write_png
+from typing import List, NamedTuple, Callable, Tuple
+from helpers.device import get_device_type, DeviceLiteral
+
+int8_iinfo = torch.iinfo(torch.int8)
+int8_range = int8_iinfo.max-int8_iinfo.min
+int8_half_range = int8_range / 2
 
 device_type: DeviceLiteral = get_device_type()
 device = torch.device(device_type)
 
-assets_dir = 'out_learn_sd1.5'
-samples_dir=path.join(assets_dir, 'samples')
-latents_dir=path.join(assets_dir, 'pt')
-test_sample_inputs_dir=path.join(assets_dir, 'test_png')
-predictions_dir=path.join(assets_dir, 'test_pred')
-science_dir=path.join(assets_dir, 'science')
-weights_dir=path.join(assets_dir, 'weights')
-for path_ in [weights_dir, predictions_dir, science_dir]:
+assets_dir = 'out_learn_wd1.3'
+samples_dir=join(assets_dir, 'samples')
+processed_train_data_dir=join(assets_dir, 'processed_train_data')
+processed_test_data_dir=join(assets_dir, 'processed_test_data')
+latents_dir=join(assets_dir, 'pt')
+test_latents_dir=join(assets_dir, 'test_pt')
+test_samples_dir=join(assets_dir, 'test_png')
+weights_dir=join(assets_dir, 'weights')
+for path_ in [weights_dir, processed_train_data_dir, processed_test_data_dir]:
   makedirs(path_, exist_ok=True)
 
-weights_path = path.join(weights_dir, 'encoder_model.pt')
+weights_path = join(weights_dir, 'approx_encoder3_wd1.3.pt')
 
-class Encoder(Module):
-  lin: Linear
-  def __init__(self) -> None:
+class Encoder3(Module):
+  in_proj: Linear
+  hidden_layers: ModuleList
+  out_proj: Linear
+  def __init__(self, hidden_layer_count: int, inner_dim: int) -> None:
     super().__init__()
-    self.lin = Linear(3, 4, True)
+    self.in_proj = Linear(3, inner_dim)
+    make_nonlin = SiLU
+    self.nonlin = make_nonlin()
+    self.hidden_layers = ModuleList([
+      layer for layer in (Linear(inner_dim, inner_dim), make_nonlin()) for _ in range(hidden_layer_count)
+    ])
+    self.out_proj = Linear(inner_dim, 4)
   
-  def forward(self, input: Tensor) -> Tensor:
-    output: Tensor = self.lin(input)
-    return output
-
-model = Encoder()
-if path.exists(weights_path):
-  model.load_state_dict(torch.load(weights_path, weights_only=True))
-model = model.to(device)
-
-training_dtype = torch.float32
-
-epochs = 30000
-
-loss_fn = MSELoss()
-optim = AdamW(model.parameters(), lr=9e-1)
-
-@dataclass
-class Dataset:
-  sample_inputs: Tensor
-  latent_outputs: Tensor
-
-def get_data() -> Dataset:
-  sample_input_paths: List[str] = fnmatch.filter(listdir(samples_dir), f"*.png")
-  # there's so few of these we may as well keep them all resident in memory
-  latent_outputs: Tensor = torch.stack([torch.load(path.join(latents_dir, sample_path.replace('.png', '.pt')), map_location=device, weights_only=True).flatten(-2).transpose(-2,-1).to(training_dtype) for sample_path in sample_input_paths])
-  images: List[IntTensor] = [read_image(path.join(samples_dir, sample_path)).to(device=device) for sample_path in sample_input_paths]
-  first, *_ = images
-  _, height, width = first.shape
-  vae_scale_factor = 8
-  scaled_height = height//vae_scale_factor
-  scaled_width = width//vae_scale_factor
-  sample_inputs: Tensor = resize(torch.stack(images), [scaled_height, scaled_width], InterpolationMode.BICUBIC).flatten(-2).transpose(-2,-1).to(training_dtype).contiguous()
-  return Dataset(
-    sample_inputs=sample_inputs,
-    latent_outputs=latent_outputs,
-  )
-
-def train(epoch: int, dataset: Dataset):
-  model.train()
-  out: Tensor = model(dataset.sample_inputs)
-  loss: Tensor = loss_fn(out, dataset.latent_outputs)
-  optim.zero_grad()
-  loss.backward()
-  optim.step()
-  if epoch % 100 == 0:
-    print(f'epoch {epoch:04d}, loss: {loss.item():.2f}')
-
-@inference_mode(True)
-def test(): pass
-#   test_sample_input_paths: List[str] = fnmatch.filter(listdir(test_sample_inputs_dir), f"*.png")
-#   test_inputs: Tensor = torch.stack([torch.load(path.join(test_sample_inputs_dir, pt), map_location=device, weights_only=True).flatten(-2).transpose(-2,-1).to(training_dtype) for sample_path in test_sample_input_paths])
-#   model.eval() # maybe inference mode does that for us
-#   predicts: Tensor = model(test_inputs)
-#   latent_height: int = 64
-#   # latent_height: int = 128
-#   # latent_width: int = 97
-#   predicts: Tensor = predicts.transpose(2,1).unflatten(2, (latent_height, -1)).contiguous()
-#   predicts: Tensor = predicts.round().clamp(0, 255).to(dtype=torch.uint8).cpu()
-#   for prediction, input_path in zip(predicts, test_input_paths):
-#     write_latents(prediction, path.join(predictions_dir, input_path.replace('pt', 'png')))
+  def forward(self, sample: Tensor) -> Tensor:
+    sample: Tensor = self.in_proj(sample)
+    sample: Tensor = self.nonlin(sample)
+    for layer in self.hidden_layers:
+      sample: Tensor = layer.forward(sample)
+    sample: Tensor = self.out_proj(sample)
+    return sample
 
 class Mode(Enum):
   Train = auto()
   Test = auto()
-  VisualizeLatents = auto()
-
 mode = Mode.Train
+test_after_train=True
+resume_training=False
+
+model = Encoder3(hidden_layer_count=1, inner_dim=12)
+if exists(weights_path) and resume_training or mode is not Mode.Train:
+  model.load_state_dict(load(weights_path, weights_only=True))
+model = model.to(device)
+
+training_dtype = torch.float32
+
+# epochs = 3000
+# epochs = 1000
+epochs = 400
+
+l2_loss = MSELoss()
+l1_loss = L1Loss()
+optim = AdamW(model.parameters(), lr=5e-2)
+
+@dataclass
+class Dataset:
+  latents: FloatTensor
+  samples: FloatTensor
+
+GetFileNames = Callable[[], List[str]]
+
+# there's so few of these we may as well keep them all resident in memory
+def get_latents(in_dir: str, out_dir: str, get_latent_filenames: GetFileNames) -> FloatTensor:
+  latents_path = join(out_dir, 'latents.pt')
+  try:
+    latents: FloatTensor = load(latents_path, map_location=device, weights_only=True)
+    return latents
+  except:
+    latents: List[FloatTensor] = [load(join(in_dir, pt), map_location=device, weights_only=True) for pt in get_latent_filenames()]
+    latents: FloatTensor = stack(latents)
+    save(latents, latents_path)
+    return latents
+  
+
+# there's so few of these we may as well keep them all resident in memory
+def get_resized_samples(in_dir: str, out_dir: str, get_sample_filenames: GetFileNames) -> IntTensor:
+  filename_stem = 'resized_samples'
+  resized_path_png = join(out_dir, f'{filename_stem}.png')
+  resized_path_pt = join(out_dir, f'{filename_stem}.pt')
+  try:
+    resized: IntTensor = load(resized_path_pt, map_location=device, weights_only=True)
+    return resized
+  except:
+    # load/resize via cv2 instead of torchvision because cv2 has INTER_AREA downsample for best PSNR.
+    # CUDA-accelerating this requires building opencv from source. instead we'll cache the result on-disk.
+    sample_filenames: List[str] = get_sample_filenames()
+    mats: List[Mat] = [cv2.imread(join(in_dir, sample_filename)) for sample_filename in sample_filenames]
+    first, *_ = mats
+    height, width, _ = first.shape
+    vae_scale_factor = 8
+    scaled_height = height//vae_scale_factor
+    scaled_width = width//vae_scale_factor
+    dsize: Tuple[int, int] = (scaled_height, scaled_width)
+    resizeds: List[Mat] = [cv2.resize(mat, dsize, interpolation=cv2.INTER_AREA) for mat in mats]
+    for img, filename in zip(resizeds, sample_filenames):
+      cv2.imwrite(join(out_dir, filename), img)
+    tensors: List[IntTensor] = [from_numpy(cvtColor(resized, cv2.COLOR_BGR2RGB)) for resized in resizeds]
+    tensor: IntTensor = stack(tensors)
+    save(tensor, resized_path_pt)
+    write_png(tensor.permute(3, 0, 1, 2).flatten(1, end_dim=2), resized_path_png)
+    return tensor.to(device)
+
+def get_data() -> Dataset:
+  get_latent_filenames: GetFileNames = lambda: fnmatch.filter(listdir(latents_dir), f"*.pt")
+  get_sample_filenames: GetFileNames = lambda: [latent_path.replace('pt', 'png') for latent_path in get_latent_filenames()]
+
+  latents: FloatTensor = get_latents(latents_dir, processed_train_data_dir, get_latent_filenames)
+  # channels-last is good for linear layers
+  latents = latents.permute(0, 2, 3, 1).contiguous()
+  latents = latents.to(training_dtype)
+  
+  samples: IntTensor = get_resized_samples(samples_dir, processed_train_data_dir, get_sample_filenames)
+  samples: FloatTensor = samples.to(training_dtype)
+  samples = samples-int8_half_range
+  samples = samples/int8_half_range
+  return Dataset(
+    latents=latents,
+    samples=samples,
+  )
+
+@dataclass
+class LossBreakdown:
+  l2: FloatTensor
+  l1: FloatTensor
+  range: FloatTensor
+  l2_scaled: FloatTensor
+  l1_scaled: FloatTensor
+  range_scaled: FloatTensor
+
+class LossComponents(NamedTuple):
+  total_loss: FloatTensor
+  breakdown: LossBreakdown
+
+def describe_loss(loss_components: LossComponents) -> str:
+  loss, b = loss_components
+  unscaled_components = f'l2: {b.l2.abs().max().item():.2f}, l1: {b.l1.abs().max().item():.2f}, r: {b.range.abs().max().item():.2f}'
+  scaled_components = f'l2: {b.l2_scaled.abs().max().item():.2f}, l1: {b.l1_scaled.abs().max().item():.2f}, r: {b.range_scaled.abs().max().item():.2f}'
+  return f'loss: {loss.item():.2f}, [u] {unscaled_components} [s] {scaled_components}'
+
+zero = zeros(1, device=device, dtype=training_dtype)
+
+def loss_fn(input: FloatTensor, target: FloatTensor) -> LossComponents:
+  # return l2_loss(input, target) + 0.05 * l1_loss(input, target) + 0.05 * (input.abs().max() - 1).clamp(min=0)**2
+  # return l2_loss(input, target) #+ 0.025 * (input.abs().max() - 1).clamp(min=0)**2
+  # return 0.9 * l2_loss(input, target) + 0.1 * l1_loss(input, target) #+ 0.025 * (input.abs().max() - 1).clamp(min=0)**2
+  l2 = l2_loss(input, target)
+  l2_scaled = 1. * l2
+  l1 = l1_loss(input, target)
+  l1_scaled = 0. * l1
+  range = sub(input.abs().max(dim=0).values, 1).clamp(min=0).mean()
+  range_scaled = 0. * range
+  breakdown = LossBreakdown(
+    l2=l2,
+    l1=l1,
+    range=range,
+    l2_scaled=l2_scaled,
+    l1_scaled=l1_scaled,
+    range_scaled=range_scaled,
+  )
+  total_loss: FloatTensor = l2_scaled + l1_scaled + range_scaled
+  return LossComponents(
+    total_loss=total_loss,
+    breakdown=breakdown,
+  )
+
+def train(epoch: int, dataset: Dataset):
+  model.train()
+  out: Tensor = model(dataset.samples)
+  loss_components = loss_fn(out, dataset.latents)
+  loss, _ = loss_components
+  optim.zero_grad()
+  loss.backward()
+  optim.step()
+  if epoch % 100 == 0:
+    loss_desc: str = describe_loss(loss_components)
+    print(f'epoch {epoch:04d}, {loss_desc}')
+
+@inference_mode(True)
+def test():
+  get_sample_filenames: GetFileNames = lambda: fnmatch.filter(listdir(test_samples_dir), f"*.png")
+  get_latent_filenames: GetFileNames = lambda: [sample_path.replace('png', 'pt') for sample_path in get_sample_filenames()]
+
+  samples: IntTensor = get_resized_samples(test_samples_dir, processed_test_data_dir, get_sample_filenames)
+  samples: FloatTensor = samples.to(training_dtype)
+  samples = samples-int8_half_range
+  samples = samples/int8_half_range
+
+  true_latents: FloatTensor = get_latents(test_latents_dir, processed_test_data_dir, get_latent_filenames)
+  true_latents = true_latents.to(training_dtype)
+
+  model.eval() # might be redundant due to inference mode but whatever
+
+  predicted_latents: Tensor = model.forward(samples)
+  # back to channels-first for comparison with true latents
+  predicted_latents = predicted_latents.permute(0, 3, 1, 2)
+  
+  loss_components = loss_fn(predicted_latents, true_latents)
+  loss_desc: str = describe_loss(loss_components)
+  print(f'validation {loss_desc}')
+
 match(mode):
   case Mode.Train:
     dataset: Dataset = get_data()
     for epoch in range(epochs):
       train(epoch, dataset)
-    torch.save(model.state_dict(), weights_path)
+    del dataset
+    save(model.state_dict(), weights_path)
+    if test_after_train:
+      test()
   case Mode.Test:
     test()
-  case Mode.VisualizeLatents:
-    input_path = '00228.4209087706.cfg07.50.pt'
-    sample: Tensor = torch.load(path.join(test_sample_inputs_dir, input_path), map_location=device, weights_only=True)
-    for ix, channel in enumerate(sample):
-      centered: Tensor = channel-(channel.max()+channel.min())/2
-      norm: Tensor = centered / centered.abs().max()
-      rescaled = (norm + 1)*(255/2)
-      write_png(rescaled.unsqueeze(0).to(dtype=torch.uint8).cpu(), path.join(science_dir, input_path.replace('pt', f'.{ix}.png')))
