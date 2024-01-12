e2cf83a226446ddeb434ee828a68f47831cfa4cc
Alex Birch <Birch-san@users.noreply.github.com>
2024-01-12 22:28:09 +0000

    document params

--- src/helpers/sampling.py
+++ src/helpers/sampling.py
@@ -1,4 +1,5 @@
 import torch
+from torch import enable_grad
 from tqdm import trange
 from typing import Optional, Literal
 
@@ -8,8 +9,22 @@ from typing import Optional, Literal
 # implements DPM-Solver++(2M), Lu et al. 2022
 # https://arxiv.org/abs/2211.01095
 @torch.no_grad()
-def sample_dpmpp_2m(model, x, sigmas, extra_args=None, callback=None, disable=None, warmup_lms: Optional[Literal['2s', 'jvp']] = None):
-    """DPM-Solver++(2M)."""
+def sample_dpmpp_2m(model, x, sigmas, extra_args=None, callback=None, disable=None, warmup_lms: Optional[Literal['2s', 'jvp']] = None, order2_until = 0):
+    """
+    DPM-Solver++(2M).
+    warmup_lms can be used to warm up the linear multistep, beginning with a second-order step instead of a Euler step.
+        the hope is to get a better approximation of the ODE trajectory at the beginning, where there is lots of uncertainty
+        and the composition is being decided.
+        2s: warm up with a DPM++(2S) ancestral step. this incurs an extra model invocation.
+        jvp: use forward-mode autodiff to determine second-order derivative. this incurs an extra two model invocations (but could be made free by training the model to output the jvp, like in Nvidia GENIE)
+
+    order2_until can be used to specify at which sigma you wish to switch to first-order steps, to reduce rainbow-style artifacting
+    (note: makes the image softer/blurrier).
+      https://github.com/crowsonkb/k-diffusion/issues/43#issuecomment-1781149170
+    to end with *two* first-order steps, use:
+      order2_until = sigmas[-3]
+    the default is to end with *one* first-order step (i.e. from the final non-zero sigma down to 0).
+    """
     extra_args = {} if extra_args is None else extra_args
     s_in = x.new_ones([x.shape[0]])
     sigma_fn = lambda t: t.neg().exp()
@@ -33,13 +48,15 @@ def sample_dpmpp_2m(model, x, sigmas, extra_args=None, callback=None, disable=No
                 x_2 = (sigma_fn(s) / sigma_fn(t)) * x - (-h * r).expm1() * denoised
                 denoised_i = model(x_2, sigma_fn(s) * s_in, **extra_args)
             elif warmup_lms == 'jvp':
-                # TODO: use forward-mode autodiff to get second derivative of ODE
-                #       like in sample_ttm:
-                # https://github.com/Birch-san/sdxl-play/blob/edf5f53999c5f49a687f151292129756de373f4f/src/sample_ttm.py#L8
-                raise RuntimeError("jvp warmup not yet implemented.")
+                with enable_grad():
+                    # TODO: use forward-mode autodiff to get second derivative of ODE
+                    #       like in sample_ttm_jvp:
+                    # https://github.com/Birch-san/sdxl-play/blob/edf5f53999c5f49a687f151292129756de373f4f/src/sample_ttm.py#L45
+                    #       (note, sample_ttm approach should work too, but may return None depending on pytorch compatibility)
+                    raise RuntimeError("jvp warmup not yet implemented.")
             else:
                 raise ValueError(f"Unsupported warmup type '{warmup_lms}'. Supported values are: ['2s', 'jvp']")
-        elif sigmas[i + 1] == 0 or old_denoised is None:
+        elif sigmas[i + 1] == 0 or old_denoised is None or sigmas[i] <= order2_until:
             denoised_i = denoised
         else:
             h_last = t - t_fn(sigmas[i - 1])
