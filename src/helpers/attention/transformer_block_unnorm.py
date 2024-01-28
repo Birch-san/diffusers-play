7dbf48ed535dea2ae06c4b9ff883d9f6bcbc6f6f
Alex Birch <Birch-san@users.noreply.github.com>
2024-01-28 01:00:56 +0000

    TransformerBlockUnNorm: delegation

--- src/helpers/attention/transformer_block_unnorm.py
+++ src/helpers/attention/transformer_block_unnorm.py
@@ -20,43 +20,43 @@ class TransformerBlockUnNorm(Module):
     ):
         # Notice that normalization is always applied before the real computation in the following blocks.
         # 1. Self-Attention
-        if self.use_ada_layer_norm:
-            norm_hidden_states = self.norm1(hidden_states, timestep)
-        elif self.use_ada_layer_norm_zero:
-            norm_hidden_states, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.norm1(
+        if self.delegate.use_ada_layer_norm:
+            norm_hidden_states = self.delegate.norm1(hidden_states, timestep)
+        elif self.delegate.use_ada_layer_norm_zero:
+            norm_hidden_states, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.delegate.norm1(
                 hidden_states, timestep, class_labels, hidden_dtype=hidden_states.dtype
             )
         else:
-            norm_hidden_states = self.norm1(hidden_states)
+            norm_hidden_states = self.delegate.norm1(hidden_states)
 
         # 0. Prepare GLIGEN inputs
         cross_attention_kwargs = cross_attention_kwargs.copy() if cross_attention_kwargs is not None else {}
         gligen_kwargs = cross_attention_kwargs.pop("gligen", None)
 
-        attn_output = self.attn1(
+        attn_output = self.delegate.attn1(
             norm_hidden_states,
-            encoder_hidden_states=encoder_hidden_states if self.only_cross_attention else None,
+            encoder_hidden_states=encoder_hidden_states if self.delegate.only_cross_attention else None,
             attention_mask=attention_mask,
             **cross_attention_kwargs,
         )
         if 'unnormed_hidden_states' in cross_attention_kwargs:
             del cross_attention_kwargs["unnormed_hidden_states"]
-        if self.use_ada_layer_norm_zero:
+        if self.delegate.use_ada_layer_norm_zero:
             attn_output = gate_msa.unsqueeze(1) * attn_output
         hidden_states = attn_output + hidden_states
 
         # 1.5 GLIGEN Control
         if gligen_kwargs is not None:
-            hidden_states = self.fuser(hidden_states, gligen_kwargs["objs"])
+            hidden_states = self.delegate.fuser(hidden_states, gligen_kwargs["objs"])
         # 1.5 ends
 
         # 2. Cross-Attention
-        if self.attn2 is not None:
+        if self.delegate.attn2 is not None:
             norm_hidden_states = (
-                self.norm2(hidden_states, timestep) if self.use_ada_layer_norm else self.norm2(hidden_states)
+                self.delegate.norm2(hidden_states, timestep) if self.delegate.use_ada_layer_norm else self.delegate.norm2(hidden_states)
             )
 
-            attn_output = self.attn2(
+            attn_output = self.delegate.attn2(
                 norm_hidden_states,
                 encoder_hidden_states=encoder_hidden_states,
                 attention_mask=encoder_attention_mask,
@@ -65,27 +65,27 @@ class TransformerBlockUnNorm(Module):
             hidden_states = attn_output + hidden_states
 
         # 3. Feed-forward
-        norm_hidden_states = self.norm3(hidden_states)
+        norm_hidden_states = self.delegate.norm3(hidden_states)
 
-        if self.use_ada_layer_norm_zero:
+        if self.delegate.use_ada_layer_norm_zero:
             norm_hidden_states = norm_hidden_states * (1 + scale_mlp[:, None]) + shift_mlp[:, None]
 
-        if self._chunk_size is not None:
+        if self.delegate._chunk_size is not None:
             # "feed_forward_chunk_size" can be used to save memory
-            if norm_hidden_states.shape[self._chunk_dim] % self._chunk_size != 0:
+            if norm_hidden_states.shape[self.delegate._chunk_dim] % self.delegate._chunk_size != 0:
                 raise ValueError(
-                    f"`hidden_states` dimension to be chunked: {norm_hidden_states.shape[self._chunk_dim]} has to be divisible by chunk size: {self._chunk_size}. Make sure to set an appropriate `chunk_size` when calling `unet.enable_forward_chunking`."
+                    f"`hidden_states` dimension to be chunked: {norm_hidden_states.shape[self.delegate._chunk_dim]} has to be divisible by chunk size: {self.delegate._chunk_size}. Make sure to set an appropriate `chunk_size` when calling `unet.enable_forward_chunking`."
                 )
 
-            num_chunks = norm_hidden_states.shape[self._chunk_dim] // self._chunk_size
+            num_chunks = norm_hidden_states.shape[self.delegate._chunk_dim] // self.delegate._chunk_size
             ff_output = torch.cat(
-                [self.ff(hid_slice) for hid_slice in norm_hidden_states.chunk(num_chunks, dim=self._chunk_dim)],
-                dim=self._chunk_dim,
+                [self.delegate.ff(hid_slice) for hid_slice in norm_hidden_states.chunk(num_chunks, dim=self.delegate._chunk_dim)],
+                dim=self.delegate._chunk_dim,
             )
         else:
-            ff_output = self.ff(norm_hidden_states)
+            ff_output = self.delegate.ff(norm_hidden_states)
 
-        if self.use_ada_layer_norm_zero:
+        if self.delegate.use_ada_layer_norm_zero:
             ff_output = gate_mlp.unsqueeze(1) * ff_output
 
         hidden_states = ff_output + hidden_states
